79b7ebb69afa845ca7b04834bab6c75600204498
Gguife <gguife747@gmail.com>
2023-11-08 15:43:38 -0300

    feat: Created post client method route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,12 @@
-const connection = require("../database/connection");
 const express = require("express");
+const router = express.Router();
 //Controllers
+const ClientController = require("../controllers/clientController")
 const CategoryController = require("../controllers/categoryController");
 
-const router = express.Router();
+//Client
+router.post("/newClient", ClientController.newClient);
+
 
 //Categories
 router.post("/newCategory", CategoryController.newCategory);
@@ -11,6 +14,7 @@ router.get("/categories", CategoryController.allCategories);
 router.get("/category/:id", CategoryController.listCategory);
 router.put("/update/category/:id", CategoryController.updateCategory);
 router.delete("/delete/category/:id", CategoryController.deleteCategory);
+
 
 
 module.exports = router;
