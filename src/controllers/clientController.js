bd27b2b3b56bc60406ceebbbd3259e58b207e07e
Gguife <gguife747@gmail.com>
2023-11-09 11:03:35 -0300

    feat: Finished client controller

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -20,14 +20,63 @@ class ClientController {
   }
 
   //GET
-  allClientsList(){
-
+  allClientsList(req, res){
+    try{
+      database.select("*").table("clients").then(client => {
+        console.log(client);
+        res.json(client);
+      })
+    }
+    catch(error){
+      console.log(error);
+      res.status(500).json({message: "Erro ao listar todos os clientes."})
+    }
   }
   //Retonar um client específico
-  clientList(){
+  clientList(req, res){
+    try{
+      const id = req.params.id;
 
+      database.select("*").table("clients").where({id:id}).then(client =>{
+        res.json(client);
+      })
+    }
+    catch(error){
+      console.log(error);
+      res.status(500).json({message: "Erro ao exibir o cliente."})
+    }
   }
 
+  //PUT
+  updateClient(req, res){
+    try{
+      const id = req.params.id;
+      const password = req.body.password;
+
+      database.where({id:id}).update({password: password}).table("clients").then(data => {
+        res.json({message: "Senha alterada com sucesso!"})
+      })
+    }
+    catch(error){
+      console.log(error);
+      res.status(500).json({message: "Erro ao atualizar cliente."})
+    }
+  }
+
+  //DELETE
+  deleteClient(req, res){
+    try{
+      const id = req.params.id;
+
+      database.where({id:id}).del().table("clients").then(data =>{
+        res.json({message: "Cliente removido com sucesso!"})
+      })
+    }
+    catch(error){
+      console.log(error);
+      res.status(500).json({message: "Erro ao deletar cliente."})
+    }
+  }
 }
 
 module.exports = new ClientController;
