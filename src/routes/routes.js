feeb192464e82ca3fa2c4780f8dcae43baea4136
Gguife <gguife747@gmail.com>
2023-11-09 17:13:46 -0300

    feat: Add product routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 //Controllers
 const ClientController = require("../controllers/clientController")
 const CategoryController = require("../controllers/categoryController");
+const ProductController = require("../controllers/productController");
 
 //Client
 router.post("/newClient", ClientController.newClient);
@@ -18,6 +19,12 @@ router.get("/category/:id", CategoryController.listCategory);
 router.put("/update/category/:id", CategoryController.updateCategory);
 router.delete("/delete/category/:id", CategoryController.deleteCategory);
 
+//Products
+router.post("/newProduct", ProductController.newProduct);
+router.get("/products", ProductController.allProductsList);
+router.get("/product/:id", ProductController.productList);
+router.put("/update/product/:id", ProductController.updateProduct);
+router.delete("/delete/product/:id", ProductController.deleteProduct);
 
 
 module.exports = router;
